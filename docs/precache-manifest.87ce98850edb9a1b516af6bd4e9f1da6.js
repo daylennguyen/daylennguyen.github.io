c94cf75d0e3a998ade32556c11bbde234a43ee75
flannyan <daylennguyen@gmail.com>
2020-05-15 18:53:17 -0700

    init

---
+++ docs/precache-manifest.87ce98850edb9a1b516af6bd4e9f1da6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8162e7b951cc76ed5d5e01d545e76eef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "298de5e4cfb69b9bef8a",
+    "url": "/static/css/main.ed9e97fe.chunk.css"
+  },
+  {
+    "revision": "f2885622005d5d5a880b",
+    "url": "/static/js/2.5b43c05f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b43c05f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298de5e4cfb69b9bef8a",
+    "url": "/static/js/main.865108b8.chunk.js"
+  },
+  {
+    "revision": "e102ee366c62aeddc484",
+    "url": "/static/js/runtime-main.54be714f.js"
+  },
+  {
+    "revision": "7d0f232e510bca16447f72c0d30b7e0b",
+    "url": "/static/media/logo.7d0f232e.svg"
+  }
+]);
