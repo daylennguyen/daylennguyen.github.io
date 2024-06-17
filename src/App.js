5c76dde897621fd03868b29bfee9a39d5b4b3c86
Daylen Nguyen <daylennguyen@gmail.com>
2024-06-17 07:56:22 +0000

    init

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-full divide-y divide-gray-200 overflow-hidden rounded-lg bg-black p-3 shadow text-white	size-full">
-      <div className="px-4 py-5 sm:px-6 flex justify-center">card header</div>
-      <div className="px-4 py-5 sm:p-6  flex justify-center h-screen">card content</div>
-      <div className="px-4 py-4 sm:px-6 flex justify-center">card footer</div>
+    <div className="h-full divide-y divide-gray-200 overflow-hidden rounded-lg bg-black p-3 text-white shadow">
+      <div className="flex justify-center px-4 py-5 sm:px-6">
+        <img
+          className="h-48 w-48 rounded-full"
+          src="./me.jpg"
+          alt=""
+        />
+      </div>
+      <div className="flex h-screen justify-center px-4 py-5 sm:p-6">
+        card content
+      </div>
+      <div className="flex justify-center px-4 py-4 sm:px-6">card footer</div>
     </div>
   );
 }
